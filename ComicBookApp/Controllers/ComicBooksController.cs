964c9e540be9a5c1a94a4699ed79b943dda023ea
Michal Janek <mikael.janek@gmail.com>
2018-01-07 16:06:30 +0100

    add separation of data from controller -> view

--- ComicBookApp/Controllers/ComicBooksController.cs
+++ ComicBookApp/Controllers/ComicBooksController.cs
@@ -10,6 +10,19 @@ namespace ComicBookApp.Controllers
     {
         public ActionResult Detail()
         {
+            ViewBag.IssueNumber = 700;
+            ViewBag.issueDescription = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
+            ViewBag.seriesTitle = "The Amazing Spider-Man";
+            ViewBag.artists = new string[]
+            {
+            "Script: Dan Slott",
+            "Pencils: Humberto Ramos",
+            "Inks: Victor Olazaba",
+            "Colors: Edgar Delgado",
+            "Letters: Chris Eliopoulos"
+                };
+
+
             return View();
         }
     }
